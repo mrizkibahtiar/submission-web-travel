8ed48962c2057c3c7aecdd5f465727746496512a
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2024-01-31 11:27:14 +0700

    navbar for mobile done

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// memunculkan nav
+const checkboxBurger = document.querySelector('.nav-mobile #menu');
+const navActive = document.querySelector('.nav-active');
+checkboxBurger.addEventListener('click', function () {
+    if (checkboxBurger.checked == true) {
+        navActive.style.transform = 'translateX(0px)';
+    } else {
+        navActive.style.transform = 'translateX(200px)';
+    }
+})
+
+
 const icon = document.querySelectorAll('#content .second a svg');
 const offer = document.querySelectorAll('#content .second a');
 
@@ -10,3 +22,5 @@ for (let i = 0; i < offer.length; i++) {
         icon[i].style.opacity = '0';
     })
 }
+
+
